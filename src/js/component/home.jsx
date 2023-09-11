86a59effa72b403751e485ca0a068b015ea5f092
AugustoSchemberger <130497560+AugustoSchemberger@users.noreply.github.com>
2023-09-11 14:44:03 +0000

    Proyecto basico todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTaskLabel, setNewTaskLabel] = useState("");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = () => {
+    fetch("https://playground.4geeks.com/apis/fake/todos/")
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data);
+      })
+      .catch((error) => console.error("Error fetching tasks:", error));
+  };
+
+  const handleAddTask = () => {
+    const newTask = { label: newTaskLabel, done: false };
+
+    fetch("https://playground.4geeks.com/apis/fake/todos/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        fetchTasks(); 
+        setNewTaskLabel(""); 
+      })
+      .catch((error) => console.error("Error adding task:", error));
+  };
+
+  const handleDeleteTask = (taskId) => {
+    fetch(`https://playground.4geeks.com/apis/fake/todos/${taskId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          const updatedTasks = tasks.filter((task) => task.id !== taskId);
+          setTasks(updatedTasks);
+        } else {
+          console.error("Error deleting task:", response.statusText);
+        }
+      })
+      .catch((error) => console.error("Error deleting task:", error));
+  };
+  
+
+  const handleResetList = () => {
+    setTasks([]);
+  };
+  
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.label}
+            <button onClick={() => handleDeleteTask(task.id)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          placeholder="Nueva tarea"
+          value={newTaskLabel}
+          onChange={(e) => setNewTaskLabel(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Agregar Tarea</button>
+      </div>
+      <button onClick={handleResetList}>Limpiar Lista</button>
+    </div>
+  );
 };
 
-export default Home;
+export default TodoList;
